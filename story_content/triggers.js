3436ad7fc2a24c1426f896d7ed7c57f12080316d
Corrine Mundorff <cbehan18@gmail.com>
2026-02-08 14:11:18 -0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,71 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5mPM6mPP8YY":
+        Script1();
+        break;
+      case "6gOwXc3SUm9":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('68MtdDPFY8q');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '5e5yZkezNCF';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5VTznwFr890');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '5cfrSIQ6FDZ';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
